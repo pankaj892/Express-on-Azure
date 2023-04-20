b35ce498d697a9ea9d77735c87158e08fdd6ab89
Pankaj biradar <pankaj28988@gmail.com>
2023-04-20 14:38:34 +0530

    Update to index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.get('/', function (req, res) { // create route
     res.send('GoodBye World!') // send response
 });
 
-if (!module.parent) { // if this is the main module
+if (!module.parent) { // check if this is the main module
     app.listen(80) // listen on port 80
     console.log('Express started on port 80') // log to console
 }
